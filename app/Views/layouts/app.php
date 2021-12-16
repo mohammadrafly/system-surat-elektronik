36866d8f9c406784f45b99c27d6b1d07286c7e9b
Mohammad Rafly <mohammadrafly19@gmail.com>
2021-12-16 18:22:25 +0700

    upload

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -2,55 +2,39 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Login EKS</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <!-- Favicons -->
-  <link href="<?php echo base_url('backend_assets/img/favicon2.png'); ?>" rel="icon">
+    <title>Login BUMDES</title>
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+    <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css'); ?>" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="<?php echo base_url('backend_assets/vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
-  <link href="<?php echo base_url('backend_assets/vendor/bootstrap-icons/bootstrap-icons.css'); ?>" rel="stylesheet">
-  <link href="<?php echo base_url('backend_assets/vendor/boxicons/css/boxicons.min.css'); ?>" rel="stylesheet">
-  <link href="<?php echo base_url('backend_assets/vendor/quill/quill.snow.css'); ?>" rel="stylesheet">
-  <link href="<?php echo base_url('backend_assets/vendor/quill/quill.bubble.css'); ?>" rel="stylesheet">
-  <link href="<?php echo base_url('backend_assets/vendor/remixicon/remixicon.css'); ?>" rel="stylesheet">
-  <link href="<?php echo base_url('backend_assets/vendor/simple-datatables/style.css'); ?>" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="<?= base_url('assets/css/sb-admin-2.min.css'); ?>" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
-  <link href="<?php echo base_url('backend_assets/css/style.css'); ?>" rel="stylesheet">
 </head>
 
-<body style="background-image: url('<?= base_url('backend_assets/img/bg-login2.png'); ?>'); background-repeat: no-repeat; background-position: center; height: 100%; background-size: cover;">
+<body class="bg-gradient-primary">
 
-  <main>
-    <div class="container">
     <?= $this->renderSection('content') ?>
-    </div>
-  </main><!-- End #main -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <!-- Bootstrap core JavaScript-->
+    <script src="<?= base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
 
-  <!-- Vendor JS Files -->
-  <script src="<?= base_url('backend_assets/vendor/apexcharts/apexcharts.min.js'); ?>"></script>
-  <script src="<?= base_url('backend_assets/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
-  <script src="<?= base_url('backend_assets/vendor/chart.js/chart.min.js'); ?>"></script>
-  <script src="<?= base_url('backend_assets/vendor/echarts/echarts.min.js'); ?>"></script>
-  <script src="<?= base_url('backend_assets/vendor/quill/quill.min.js'); ?>"></script>
-  <script src="<?= base_url('backend_assets/vendor/simple-datatables/simple-datatables.js'); ?>"></script>
-  <script src="<?= base_url('backend_assets/vendor/tinymce/tinymce.min.js'); ?>"></script>
-  <script src="<?= base_url('backend_assets/vendor/php-email-form/validate.js'); ?>"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="<?= base_url('assets/vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
 
-  <!-- Template Main JS File -->
-  <script src="<?= base_url('backend_assets/js/main.js'); ?>"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="<?= base_url('assets/js/sb-admin-2.min.js'); ?>"></script>
 
 </body>
 
--- app/Views/superadmin/user.php
+++ app/Views/superadmin/user.php
@@ -3,92 +3,84 @@
 <?= $this->section('content') ?>
 
     <div class="pagetitle">
-      <h1>Data User</h1>
+      <h1>Master User</h1>
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="<?= base_url(); ?>/superadmin">Dashboard</a></li>
-          <li class="breadcrumb-item">Data User</li>
+          <li class="breadcrumb-item">Master User</li>
         </ol>
       </nav>
     </div><!-- End Page Title -->
 
-    <section class="section">
-      <div class="row">
-        <div class="col-lg-12">
-
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">List User</h5>
-              <?php
-                if(session()->getFlashData('message')){
-                ?>
-                  <div class="alert alert-success alert-dismissible fade show" role="alert">
-                      <?= session()->getFlashData('message') ?>
-                      <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
-                  </div>
-                <?php
-                }
-                ?>
-              <?php
-                if(session()->getFlashData('berhasil')){
-                ?>
-                  <div class="alert alert-success alert-dismissible fade show" role="alert">
-                      <?= session()->getFlashData('berhasil') ?>
-                      <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
-                  </div>
-                <?php
-                }
-                ?>
-              <!-- Table with stripped rows -->
-              <a href="<?= base_url('superadmin/tambah'); ?>"><span class="badge bg-secondary mb-3">Tambah</span></a>
-              <table class="table table-bordered" id="users-list">
-                <thead>
-                    <tr>
-                      <th>No</th>
-                      <th>Id</th>
-                      <th>Username</th>
-                      <th>Nama</th>
-                      <th>Nomor HP</th>
-                      <th>Role</th>
-                      <th>Email</th>
-                      <th>Join</th>
-                      <th>Opsi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php if($user): ?>
-                    <?php 
-                      $no = 1;
-                      foreach($user as $row): ?>
-                    <tr>
-                      <td><?php echo $no++; ?></td>
-                      <td><?php echo $row['id']; ?></td>
-                      <td><?php echo $row['username']; ?></td>
-                      <td><?php echo $row['name']; ?></td>
-                      <td><?php echo $row['phone_no']; ?></td>
-                      <td><?php echo $row['role']; ?></td>
-                      <td><?php echo $row['email']; ?></td>
-                      <td><?php echo $row['created_at']; ?></td>
-                      <td>
-                                <a href="<?= base_url('superadmin/user/edit/'.$row['id']); ?>">
-                                  <span class="badge bg-primary">edit</span>
-                                </a>
-                                <a href="<?= base_url('superadmin/user/delete/'.$row['id']); ?>">
-                                  <span class="badge bg-danger">hapus</span>
-                                </a>
-                      </td>
-                    </tr>
-                  <?php endforeach; ?>
-                  <?php endif; ?>
-                </tbody>
-              </table>
-              <!-- End Table with stripped rows -->
-
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </section>
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary"></h6>
+                        </div>
+                        <div class="card-body">
+                        <?php
+                        if(session()->getFlashData('message')){
+                        ?>
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <?= session()->getFlashData('message') ?>
+                            <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        <?php
+                        }
+                        ?>
+                        <?php
+                        if(session()->getFlashData('berhasil')){
+                        ?>
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <?= session()->getFlashData('berhasil') ?>
+                            <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        <?php
+                        }
+                        ?>
+                        <a href="<?= base_url('superadmin/tambah'); ?>"><span class="btn btn-secondary mb-3">Tambah</span></a>
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>ID User</th>
+                                            <th>Username</th>
+                                            <th>Email</th>
+                                            <th>Role</th>
+                                            <th>Nomor HP</th>
+                                            <th>Join</th>
+                                            <th>Opsi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php if($user): ?>
+                                        <?php 
+                                        $no = 1;
+                                        foreach($user as $row): ?>
+                                        <tr>
+                                            <td><?php echo $no++; ?></td>
+                                            <td><?php echo $row['id']; ?></td>
+                                            <td><?php echo $row['username']; ?></td>
+                                            <td><?php echo $row['email']; ?></td>
+                                            <td><?php echo $row['role']; ?></td>
+                                            <td><?php echo $row['phone_no']; ?></td>
+                                            <td><?php echo $row['created_at']; ?></td>
+                                            <td>
+                                                <a href="<?= base_url('superadmin/user/edit/'.$row['id']); ?>">
+                                                    <span class="btn btn-primary btn-sm">edit</span>
+                                                </a>
+                                                <a href="<?= base_url('superadmin/user/delete/'.$row['id']); ?>">
+                                                    <span class="btn btn-danger btn-sm">hapus</span>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <?php endforeach; ?>
+                                        <?php endif; ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
 
 <?= $this->endSection() ?>
